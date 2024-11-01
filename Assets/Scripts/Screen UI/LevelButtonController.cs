91ab5bed5ec2bbcae15a7a050f90ecd5f063e8e2
WEIHUA ZHANG <weihuazhang@WEIHUAdeMacBook-Pro.local>
2024-11-01 20:01:18 +1100

    Completed 50% of UI setup, added High Score, Time display, and background music loop

--- Assets/Scripts/Screen UI/LevelButtonController.cs
+++ Assets/Scripts/Screen UI/LevelButtonController.cs
@@ -6,12 +6,6 @@ public class LevelButtonController : MonoBehaviour
     // Level 1 按钮的点击事件
     public void LoadLevel1()
     {
-        SceneManager.LoadScene("Level1");  // 确保场景名称与场景管理器中的名称一致
-    }
-
-    // Level 2 按钮的点击事件
-    public void LoadLevel2()
-    {
-        SceneManager.LoadScene("Level2");  // 确保场景名称与场景管理器中的名称一致
+        SceneManager.LoadScene("Level1");  // 确保场景名称与项目中 Level1 场景的名称一致
     }
 }
